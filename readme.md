07720790be5b20a51ab7cf48e219c59691c0901f
RenanKr4t0s <redrums88@gmail.com>
2024-07-27 15:45:01 -0300

    10 horas de alteracoes, reestruturacao, limpeza, reedicao

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Criar esse Readme
+
+### Fun Facts do Desenvolvimento
+História cartão antes da página
+
+Mencionar todos os sites
+Colocar a referencia do icons8.com
+Colocar a referencia do https://devicon.dev/
+"https://icons8.com/icon/sWf0uqytq2d8/whatsapp"
+"https://icons8.com"
+Api do whatsapp também
+
+https://ezgif.com/video-to-gif/ezgif-4-13cd4496e8.mp4
+
+tive bastante trabalho reorganizando todo o código
--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let navburguer = document.getElementById('navburguer')
-let list = document.querySelector('[data-list]');
+const navburguer = document.getElementById('navburguer')
+const list = document.querySelector('[data-list]');
+
 
 function openHamburguer(){
   navburguer.classList.toggle("hider");
   list.classList.toggle('list-hider');
 }
+
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const carouselContent = document.querySelector(".carousel-content");
+  const icons = carouselContent.innerHTML;
+  carouselContent.innerHTML += icons + icons;
+});
